76e6dea70dbcca08525b8114878d6918faaa8158
Mariam Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2022-09-20 10:40:56 -0500

    Rutas de Servicios

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ import { Main } from "./styles/styles";
 import GlobalStyles from "./styles/GlobalStyles";
 import LoaderContext from "./components/Loader/LoaderContext";
 import SectionsWrapper from "./components/SectionsWrapper";
+import TransformacionDigital from "./components/Services/TransformacionDigital"
+import GestionConocimiento from "./components/Services/GestionConocimiento"
+import Plataforma4 from "./components/Services/GestionConocimiento/Plataforma4"
+import InstructivosRA from "./components/Services/GestionConocimiento/InstructivosRA"
+import DigitalizacionProcesos from "./components/Services/DigitalizacionProcesos"
+import LaboratoriosWeb from "./components/Services//DigitalizacionProcesos/LaboratoriosWeb"
+import AnalisisCienciaDatos from "./components/Services/AnalisisCienciaDatos"
+import DataScience from "./components/Services/AnalisisCienciaDatos/DataScience"
 
 export default function App() {
   return (
@@ -28,7 +36,40 @@ export default function App() {
             <Header/>           
             <Switch>
               <Route exact path="/" component={SectionsWrapper} />
-              <Route path="/contact" component={AboutPage} />
+              <Route
+        path="/servicios/transformacion-digital"
+        component={TransformacionDigital}
+      />
+      <Route
+        path="/servicios/gestion-del-conocimiento"
+        component={GestionConocimiento}
+      />
+      {/* ------ Inicio Gestion Conocimiento ------ */}
+      <Route
+        path="/servicios/plataforma-de-gestión-del-conocimiento-industrial-4-0"
+        component={Plataforma4}
+      />
+      <Route
+        path="/servicios/instructivos-de-mantenimiento-con-realidad-aumentada"
+        component={InstructivosRA}
+      />
+      {/* ------ Fin Gestion Conocimiento ------ */}
+      <Route
+        path="/servicios/digitalizacion-de-procesos"
+        component={DigitalizacionProcesos}
+      />
+
+      {/* ------ digitalizacion-de-procesos ------ */}
+      <Route path="/servicios/laboratorios-web" component={LaboratoriosWeb} />
+      {/* ------ Fin digitalizacion-de-procesos ------ */}
+
+      <Route
+        path="/servicios/analitica-y-ciencia-de-datos"
+        component={AnalisisCienciaDatos}
+      />
+      {/* ------ Inicio Analitica y ciencia de datos ------ */}
+      <Route path="/servicios/data-science" component={DataScience} />
+      {/* ------ Fin Gestion Analitica y ciencia de datos ------ */}
             </Switch>
             <GlobalStyles />
         </Router>
